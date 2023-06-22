21db58548b84ab349cc6602e452eb34edeb55e6e
ShubhSharma03 <137335462+ShubhSharma03@users.noreply.github.com>
2023-06-22 11:35:45 +0530

    Add files via upload

---
+++ Controllers/IssueDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace trackingapi.Controllers
+{
+    public class modelIssueDbContext
+    {
+        public modelIssueDbContext(object issues)
+        {
+            Issues = issues;
+        }
+
+        public object Issues { get; internal set; }
+    }
+}
