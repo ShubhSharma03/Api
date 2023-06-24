e7e0bea22c187d944f00747c4ff5004e77b0f995
ShubhSharma03 <137335462+ShubhSharma03@users.noreply.github.com>
2023-06-24 20:48:13 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeControllerTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public string Index()
+        {
+            return "It's a Home Page";
+        }
+    }
+}
